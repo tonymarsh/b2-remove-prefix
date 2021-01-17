0c5ee40022b87518f443c33cea2fa2e5253532c1
Phillip Stromberg <phillip@strombergs.com>
2021-01-17 13:43:23 -0600

    Documented code and cleaned up debug outputs

--- index.js
+++ index.js
@@ -57,28 +57,49 @@ const ERROR_CODES = {
     "505": "HTTP Version Not Supported",
 }
 
-
+// entrypoint
 addEventListener('fetch', event => {
-    event.respondWith(handleRequest(event.request))
+    event.respondWith(handleRequest(event))
 })
 
 
-async function addSecurityHeaders(response) {
+/**
+ * Add extra security headers to get an A on https://securityheaders.com/
+ *
+ * @param response a Response object that will be modified in-place
+ */
+function addSecurityHeaders(response) {
     let contentType = response.headers.get("Content-Type")
 
     // if HTML content
     if(/text\/html/i.test(contentType)) {
+        // "unsafe-inline" for style-src doesn't seem like that big a deal
+        // but it will prevent you from getting an A+ on https://securityheaders.com/
         response.headers.set("Content-Security-Policy", `default-src 'self'; style-src 'unsafe-inline'; frame-ancestors 'self'`)
+        // forbid geolocation and microphone usage by this page or any iframes on it
         response.headers.set("Permissions-Policy", `geolocation=(),microphone=()`)
         response.headers.set("Referrer-Policy", "no-referrer-when-downgrade")
         response.headers.set("X-Frame-Options", "SAMEORIGIN")
     }
     else {
+        // allow hotlinking of font files and other assets
         response.headers.set("Access-Control-Allow-Origin", '*');
     }
 }
 
 
+/**
+ * Fetch and return a file from Backblaze B2 by supplying the download authorization
+ * token in the Authorization header.
+ * Errors are rewritten to not give away that this is a B2 bucket.
+ * Headers starting with "x-bz" are removed before being returned to the user.
+ * Adds cache and security-related headers.
+ *
+ * @param request the request from a client that will be rewritten to fetch from
+ *                a Backblaze B2 bucket
+ * @returns {Promise<Response>} the Response to the client that will either be
+ *                              the requested file or an error page
+ */
 async function getB2File(request) {
     const downloadAuth = await B2CDN.get("downloadAuth")
     const downloadUrl = await B2CDN.get("downloadUrl")
@@ -106,6 +127,14 @@ async function getB2File(request) {
 }
 
 
+/**
+ * Adds cache headers, converts some Backblaze B2-specific headers to standard
+ * headers, and deletes the headers that start with "x-bz"
+ *
+ * @param request a request for a file on B2
+ * @param response the successful response from B2 that will be copied and modified
+ * @returns {Promise<Response>} the modified response
+ */
 async function modifiedB2Response(request, response) {
     console.log("modifiedB2Response...")
     const newResponse = new Response(response.body, response)
@@ -120,6 +149,15 @@ async function modifiedB2Response(request, response) {
 }
 
 
+/**
+ * Converts the x-bz-content-sha1 header to an ETag header.
+ * Converts the x-bz-upload-timestamp header to a Last-Modified header.
+ * By default also deletes all headers that start with "x-bz"
+ *
+ * @param request the request from the client for a B2 file
+ * @param response the response from B2 that will be modified in-place
+ * @param deleteHeaders if true, delete the x-bz headers in the response
+ */
 function convertB2Headers(request, response, deleteHeaders=true) {
     console.log("convertB2Headers...")
     // get a Last-Modified header from x-bz-upload-timestamp
@@ -153,12 +191,21 @@ function convertB2Headers(request, response, deleteHeaders=true) {
 }
 
 
+/**
+ * Rewrite error responses from Backblaze B2 to have fun little faces. This
+ * obfuscates the fact that Backblaze B2 is being used and is generally a
+ * more succinct error page for end users.
+ *
+ * @param request the request from the client to B2
+ * @param response the response from B2 that will be copied and modified
+ * @returns {Promise<Response>} the new response with the fun face
+ */
 async function rewriteErrorResponse(request, response) {
     console.log("rewriteErrorResponse...")
     console.log("Original error response:")
     console.log(response)
 
-    // pick a random face from our ERROR_FACES array at the top
+    // pick a random face from our ERROR_FACES array
     const randomIdx = Math.floor(Math.random() * ERROR_FACES.length)
     const randomFace = ERROR_FACES[randomIdx]
     const statusText = ERROR_CODES[response.status]
@@ -173,35 +220,44 @@ async function rewriteErrorResponse(request, response) {
         }
     })
 
-    // CACHE.put(request.clone(), newResponse.clone())
-
-    console.log("Returning newResponse!")
     return newResponse
 }
 
 
-async function handleRequest(request) {
+/**
+ * Handle an incoming user request.
+ *
+ * @param event the fetch event that triggered this listener
+ * @returns {Promise<*>} a response either from Backblaze B2 or an error message
+ */
+async function handleRequest(event) {
+    const request = event.request
+
+    // return from cache if we've seen this before and the response isn't stale
     const cachedResponse = await CACHE.match(request.clone())
     if(cachedResponse !== undefined) {
         return cachedResponse
     }
+
     const r = new Router()
     // Replace with the appropriate paths and handlers
     // r.get('.*/bar', () => new Response('responding for /bar'))
     // r.get('.*/foo', request => handler(request))
-    // r.post('.*/foo.*', request => handler(request))
-    // r.get('/demos/router/foo', request => fetch(request)) // return the response from the origin
-    // r.get('/', () => new Response(":)")) // return a default message for the root route
-    // r.get('/robots\\.txt', request => getRobotsTxt(request))
+
+    // display the possible error message faces when a user visits /faces or /faces.txt
     r.get("/faces(\\.txt)?", () => new Response(`${ERROR_FACES.join(" \n")}`, {
         headers: {"Content-Type": PLAIN_TEXT_CONTENT_TYPE}
     }))
+
+    // catch-all route to return a Backblaze B2 file (should be last router rule)
     r.get('.*', request => getB2File(request))
 
-
+    // evaluate this request and get the response from the matching route handler
     const response = await r.route(request)
     addSecurityHeaders(response)
-    console.log("ABOUT TO CACHE REQUEST:")
-    CACHE.put(request.clone(), response.clone())
+
+    // put this response in the cache in the background (the Worker will stay alive to finish the job)
+    event.waitUntil(CACHE.put(request.clone(), response.clone()));
+
     return response
 }
