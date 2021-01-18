43687f767da3f59681f34db6dc95a67db74175b4
Phillip Stromberg <phillip@strombergs.com>
2021-01-18 01:43:13 -0600

    Directory listing added

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ const ERROR_FACES = [
 
 const CACHE_AGE_SECONDS = 604800 // 1 week in seconds
 const CACHE_ERRORS_SECONDS = 10  // cache errors like 404 for 10 seconds
+const CACHE_DIR_SECONDS = 30  // cache a directory listing for 10 seconds
 const PLAIN_TEXT_CONTENT_TYPE = "text/plain;charset=utf-8"
+const HTML_CONTENT_TYPE = "text/html;charset=utf-8"
 
 const ERROR_CODES = {
     "400": "Bad Request",
@@ -57,6 +59,8 @@ const ERROR_CODES = {
     "505": "HTTP Version Not Supported",
 }
 
+const B2_LIST_FILE_NAMES_ENDPOINT = "/b2api/v2/b2_list_file_names"
+
 // entrypoint
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event))
@@ -75,7 +79,7 @@ function addSecurityHeaders(response) {
     if(/text\/html/i.test(contentType)) {
         // "unsafe-inline" for style-src doesn't seem like that big a deal
         // but it will prevent you from getting an A+ on https://securityheaders.com/
-        response.headers.set("Content-Security-Policy", `default-src 'self'; style-src 'unsafe-inline'; frame-ancestors 'self'`)
+        // response.headers.set("Content-Security-Policy", `default-src 'self'; style-src 'unsafe-inline' 'self' https://cdnjs.cloudflare.com; frame-ancestors 'self'`)
         // forbid geolocation and microphone usage by this page or any iframes on it
         response.headers.set("Permissions-Policy", `geolocation=(),microphone=()`)
         response.headers.set("Referrer-Policy", "no-referrer-when-downgrade")
@@ -126,6 +130,136 @@ async function getB2File(request) {
     }
 }
 
+async function getB2Directory(request) {
+    console.log("getB2Directory...")
+    const apiUrl = await B2CDN.get("apiUrl")
+    const authToken = await B2CDN.get("authToken")
+    const bucketId = await B2CDN.get("bucketId")
+
+    const url = new URL(apiUrl)
+    url.pathname = B2_LIST_FILE_NAMES_ENDPOINT
+    const requestedUrl = new URL(request.url)
+    console.log(`requestedUrl.pathname = ${requestedUrl.pathname}`)
+    const prefix = requestedUrl.pathname.substring(1)  // chop off first / character
+    console.log(`prefix = ${prefix}`)
+
+    const requestBody = {
+        bucketId: bucketId,
+        maxFileCount: 10000,
+        prefix: prefix,
+        delimiter: "/"
+    }
+
+    const response = await fetch(url.toString(), {
+        method: "POST",
+        headers: {
+            "Authorization": authToken,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody)
+    })
+
+    const htmlResponse = await convertListFileNamesToHTML(request, response)
+
+    htmlResponse.headers.set("Cache-Control", `public, immutable, max-age=${CACHE_DIR_SECONDS}`)
+    htmlResponse.headers.set("Expires", new Date(Date.now() + CACHE_DIR_SECONDS * 1000).toUTCString())
+    // return new Response(response.body, response)
+    return htmlResponse
+}
+
+
+async function convertListFileNamesToHTML(request, response) {
+    console.log("convertListFileNamesToHTML...")
+    const respJson = await response.json()
+    const requestUrl = new URL(request.url)
+    const fullPath = requestUrl.pathname.substring(1)
+    let currentDir = requestUrl.pathname.substring(1).match(/([^/]+)\/$/)
+    if(currentDir) {
+        currentDir = currentDir[1]
+    }
+    else {
+        currentDir = "/"
+    }
+    const prefixLength = fullPath.length
+
+    let listings = ''
+    if(prefixLength > 0) {
+        listings = HTML_LINE_ITEM("..", "Up a Level", "", "")
+    }
+
+    const folders = []
+    const files = []
+
+    // make sure folders show up first
+    for(const file of respJson.files) {
+        if(/(^\.bzEmpty|\/\.bzEmpty)$/.test(file.fileName)) {
+            // skip .bzEmpty files which are there to help create "folders"
+        }
+        else if(file.action === "folder") {
+            folders.push(file)
+        }
+        else {
+            files.push(file)
+        }
+    }
+
+    // check if we received zero results. If so, this folder didn't exist
+    // so return a 404
+    if(!(folders.length || files.length)) {
+        let errorResponse = new Response("", {status: 404})
+        return rewriteErrorResponse(request, errorResponse)
+    }
+
+    for(const fldr of folders) {
+        listings += convertFileInfoJsonToHTML(fldr, prefixLength)
+    }
+    for(const file of files) {
+        listings += convertFileInfoJsonToHTML(file, prefixLength)
+    }
+
+    let html = FILE_LIST_HTML_TEMPL(currentDir, fullPath, listings)
+    const htmlResponse = new Response(html, {
+        status: 200,
+        statusText: "OK",
+        headers: {
+            "Content-Type": HTML_CONTENT_TYPE,
+        }
+    })
+    return htmlResponse
+}
+
+
+function convertFileInfoJsonToHTML(file, prefixLength) {
+    let basename = file.fileName.substring(prefixLength)
+    let dateStr = "", size = ""
+    if(file.action !== "folder") {
+        let ts = new Date(file.uploadTimestamp)
+        // dateStr = `${ts.toDateString()} ${ts.toLocaleTimeString()}`
+        dateStr = ts.toUTCString()
+        size = file.contentLength
+        if(size > 1099511627776) {  // 1 TiB
+            size = (size / 1099511627776).toFixed(2)
+        }
+        else if(size > 1073741824) {  // 1 GiB
+            size = (size / 1073741824).toFixed(2)
+            size = `${size} GiB`
+        }
+        else if(size > 1048576) {  // 1 MiB
+            size = (size / 1048576).toFixed(1)
+            size = `${size} MiB`
+        }
+        else if(size > 4096) {  // 4 KiB
+            size = (size / 1024).toFixed(1)
+            size = `${size} KiB`
+        }
+        else {
+            size = `${size} B`
+        }
+    }
+
+    return HTML_LINE_ITEM(basename, basename, size, dateStr, file.action)
+}
+
 
 /**
  * Adds cache headers, converts some Backblaze B2-specific headers to standard
@@ -133,17 +267,19 @@ async function getB2File(request) {
  *
  * @param request a request for a file on B2
  * @param response the successful response from B2 that will be copied and modified
+ * @param convertHeaders if true, convert x-bz headers to standard headers then delete them
  * @returns {Promise<Response>} the modified response
  */
-async function modifiedB2Response(request, response) {
+async function modifiedB2Response(request, response, convertHeaders=true) {
     console.log("modifiedB2Response...")
     const newResponse = new Response(response.body, response)
 
     // cache for a week
     newResponse.headers.set("Cache-Control", `public, immutable, max-age=${CACHE_AGE_SECONDS}`)
     newResponse.headers.set("Expires", new Date(Date.now() + CACHE_AGE_SECONDS * 1000).toUTCString())
-
-    convertB2Headers(request, newResponse)
+    if(convertHeaders) {
+        convertB2Headers(request, newResponse)
+    }
 
     return newResponse
 }
@@ -234,10 +370,10 @@ async function handleRequest(event) {
     const request = event.request
 
     // return from cache if we've seen this before and the response isn't stale
-    const cachedResponse = await CACHE.match(request.clone())
-    if(cachedResponse !== undefined) {
-        return cachedResponse
-    }
+    // const cachedResponse = await CACHE.match(request.clone())
+    // if(cachedResponse !== undefined) {
+    //     return cachedResponse
+    // }
 
     const r = new Router()
     // Replace with the appropriate paths and handlers
@@ -248,7 +384,7 @@ async function handleRequest(event) {
     r.get("/faces(\\.txt)?", () => new Response(`${ERROR_FACES.join(" \n")}`, {
         headers: {"Content-Type": PLAIN_TEXT_CONTENT_TYPE}
     }))
-
+    r.get('.*/', request => getB2Directory(request))
     // catch-all route to return a Backblaze B2 file (should be last router rule)
     r.get('.*', request => getB2File(request))
 
@@ -260,4 +396,87 @@ async function handleRequest(event) {
     event.waitUntil(CACHE.put(request.clone(), response.clone()));
 
     return response
+}
+
+const FILE_LIST_HTML_TEMPL = (currentDir, fullPath, listings) => `<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>${currentDir}</title>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" 
+      integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" 
+      crossorigin="anonymous" />
+    <link rel="stylesheet" href="/s/css/bootstrap-icons.css" />
+  </head>
+  <body class="bg-light">
+    <div class="container">
+  <div class="py-5 text-center">
+    <h2>Directory Listing of ${currentDir}</h2>
+    <p class="lead">${fullPath}</p>
+  </div>
+
+  <div class="row">
+    <div class="col-md-12">
+      <table class="table">
+        <thead class="thead-light">
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Size</th>
+            <th scope="col">Uploaded</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${listings}
+        </tbody>
+      </table>
+    </div>
+  </div>
+
+  <footer class="my-5 pt-5 text-muted text-center text-small">
+    <p class="mb-1">Just hostin' wholesome content</p>
+    <ul class="list-inline">
+      <li class="list-inline-item"><a href="/meme/">Memes</a></li>
+    </ul>
+  </footer>
+</div>
+</body>
+</html>
+`
+
+
+const HTML_LINE_ITEM = (link, basename, size, uploaded, action) => {
+    let icon
+    if(link === "..") {
+        icon = "arrow-90deg-up"
+    }
+    else if(action === "folder") {
+        icon = "folder"
+    }
+    else if(/\.(jpe?g|png|bmp|tiff?|gif|webp|tga|cr2|nef|ico)$/i.test(basename)) {
+        icon = "file-image"
+    }
+    else if(/\.(pub|txt|ini|cfg|css|js)$/i.test(basename)) {
+        icon = "file-text"
+    }
+    else if(/\.(mp4|mkv|wmv|flv|hls|ogv|avi)$/i.test(basename)) {
+        icon = "file-play"
+    }
+    else if(/\.(mp3|wma|flac|ogg|aac|m4a)$/i.test(basename)) {
+        icon = "file-music"
+    }
+    else if(/\.(zip|tgz|gz|tar|7z|rar|xz)$/i.test(basename)) {
+        icon = "file-zip"
+    }
+    else {
+        icon = "file"
+    }
+    return `
+<tr>
+    <th scope='row'><a href='${link}'><i class='bi bi-${icon}'></i> ${basename}</a></th>
+    <td>${size}</td>
+    <td class='date-field'>${uploaded}</td>
+</tr>
+`
 }
